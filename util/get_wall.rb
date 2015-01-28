021c6bfe3aac99d1ab34fc71d359f2e57d7783ed
David Barnett <david@barnett.net.nz>
2015-01-29 08:36:48 +1300

    fixed get_wall.rb

--- util/get_wall.rb
+++ util/get_wall.rb
@@ -2,11 +2,9 @@
 
 WPPATH=File.expand_path "~/Pictures/wallpapers/"
 
-WPS = Dir.entries(WPPATH)
+Dir.chdir(WPPATH)
+WPS = Dir.glob("*{png,jpg,jpeg}")
 TIME_OUT = 30*60
-
-# Remove '.' and '..' and '.sync'
-WPS.shift(3)
 
 index = ((Time.now.to_i)/TIME_OUT) % WPS.size
 puts File.join(WPPATH , WPS[index])
